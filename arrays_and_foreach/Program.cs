74ce38a9f8d2337e2c0b09370f740432d235a578
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-28 06:59:35 +0100

    added multi dim arrays

--- arrays_and_foreach/Program.cs
+++ arrays_and_foreach/Program.cs
@@ -25,6 +25,42 @@ namespace arrays_and_foreach
             }
             Console.ReadKey();
 
+            //declaring 2d array
+            string[,] matrix;
+
+            //declaring 3d array
+            int[,,] three3;
+
+            int[,] array2D = new int[,]
+            {
+                {1,2,3 },
+                {4,5,6},
+                {7,8,9 }
+            };
+            //accessing 5
+            Console.WriteLine("central value is {0}", array2D[2, 2]);
+            Console.ReadKey();
+
+            //changing the value
+            string[,] array2DString = new string[3, 2]
+            {
+                {"one","two" },
+                {"three" ,"four"},
+                {"five","six" }
+            };
+            Console.WriteLine(array2DString[1, 1]);
+
+            array2DString[1, 1] = "chicken";
+            Console.WriteLine(array2DString[1, 1]);
+
+            Console.ReadKey();
+
+            //checking the number of dimentions
+
+            int dimentions = array2DString.Rank;
+            Console.WriteLine(dimentions);
+            Console.ReadKey();
+
         }
     }
 }
