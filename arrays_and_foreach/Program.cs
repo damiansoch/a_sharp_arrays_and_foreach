acbeed1b6eb86ad08859926d3de4b3b491aefb2a
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-28 06:31:40 +0100

    Add project files.

---
+++ arrays_and_foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace arrays_and_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = { "john", "ana", "bill", "rory", "stephen", "ed", };
+
+            foreach (string friend in friends)
+            {
+                Console.WriteLine("name: {0}", friend);
+
+            }
+            Console.ReadKey();
+
+            string text = "Whatever 5";
+
+            foreach (char letter in text)
+            {
+                Console.WriteLine(letter);
+                Console.WriteLine(char.IsLetter(letter));
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
